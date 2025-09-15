8300e05ec418e664fb21f5dc1b418da18c220988
khaledrazouk <94979673+khaledrazouk@users.noreply.github.com>
2025-09-15 16:04:17 +0200

    Add app_containerapp/examples/acr/versions.tf

---
+++ app_containerapp/examples/acr/versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.20.0, < 5.0"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
