5d3626517c245ccaa49131b6ee2eb0b5ccc0f365
khaledrazouk <94979673+khaledrazouk@users.noreply.github.com>
2025-09-15 16:04:26 +0200

    Add app_containerapp/examples/dapr/versions.tf

---
+++ app_containerapp/examples/dapr/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">= 2.4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.20.0, < 5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
